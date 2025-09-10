7548b8c4fda7f65799328d9ad37f03823bf3e6ba
TomOtter <102150816+TomOtter@users.noreply.github.com>
2025-09-10 12:12:59 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Loughboorough Community Kitchen";
+export const SITE_DESCRIPTION = "Making a difference in the community by providing meals to those in need!";
