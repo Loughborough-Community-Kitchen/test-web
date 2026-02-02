736848b2df198510af4a41a7cca03c92acb75e8e
TomOtter <102150816+TomOtter@users.noreply.github.com>
2026-02-02 22:59:26 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://loughboroughcommunitykitchen.org.uk",
   
   integrations: [mdx(), sitemap()],
-  
-  adapter: cloudflare({
-    platformProxy: {enabled: true},
-    mode: "directory",
-  }),
 });
