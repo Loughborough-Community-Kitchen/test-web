3642a81423118e3af23b2fb3a99558f394220d07
TomOtter <102150816+TomOtter@users.noreply.github.com>
2026-02-07 16:29:25 +0000

    Delete functions/api/contact/index.ts

--- functions/api/contact/index.ts
+++
@@ -1,7 +0,0 @@
-export async function onRequestPost({ request }) {
-  const data = await request.formData();
-  return new Response(JSON.stringify({ received: true }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
