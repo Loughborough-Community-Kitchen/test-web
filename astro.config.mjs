2c49b11ac87b9e2ca7390c7d2f5bb138857757ff
TomOtter <102150816+TomOtter@users.noreply.github.com>
2026-02-07 14:12:32 +0000

    Add Cloudflare adapter to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://loughboroughcommunitykitchen.org.uk",
+
+  output: "server",
+  adapter: cloudflare({
+    mode: "pages",
+    platformProxy: { enabled: true },
+  }),
+
   
   integrations: [mdx(), sitemap()],
 });
+
+
+
+
